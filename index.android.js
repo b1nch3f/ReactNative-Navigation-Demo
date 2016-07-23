a0b56047d82794005e60f258dc96248b0473ffb5
sangram kesari ray <shankar.ray030@gmail.com>
2016-07-23 16:26:36 +0530

    api demo

--- index.android.js
+++ index.android.js
@@ -2,7 +2,6 @@
  
 var React = require('react');
 var ReactNative = require('react-native');
-var formatTime = require('minutes-seconds-milliseconds');
  
 var {
   Alert,
@@ -12,158 +11,53 @@ var {
   View,
   StyleSheet
 } = ReactNative;
+
+var region = {
+  latitude: 0,
+  longitude: 0
+}
+
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=xxxx';
+
+var url = `${rootUrl}&lat=${region.latitude}&lon=${region.longitude}`;
  
 var sample = React.createClass({
-  getInitialState: function() {
-    return {
-      timeElapsed: null,
-      running: false,
-      startTime: null,
-      laps: []
-    }
-  },
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-
-          <View style={styles.timerWrapper}> 
-            <Text style={styles.timer}>
-              {formatTime(this.state.timeElapsed)}
-            </Text>
-          </View>
-
-          <View style={styles.bottomWrapper}> 
-            {this.startStopButton()}
-            {this.lapButton()}
-          </View> 
-
-        </View> 
-
-        <View style={styles.footer}>
-          {this.laps()}
-        </View>
-        
-      </View>
-    );
-  },
-  laps: function() {
-    return this.state.laps.map(function(time, index) {
-      return(
-        <View style={styles.lap} key={index}>
-          <Text style={styles.lapText}>
-            Lap #{index +1}
-          </Text>
-          <Text style={styles.lapText}>
-            {formatTime(time)}
-          </Text>
-        </View>
-      );
-    });
-  },
-  startStopButton: function() {
-    var style = this.state.running ? styles.stopButton : styles.startButton;
-    return(
-      <TouchableHighlight 
-        underlayColor={'grey'}
-        onPress={this.handleStartPress}
-        style={[styles.button, style]}
-        >
-        <Text>
-          {this.state.running ? 'Stop' : 'Start'}
-        </Text>
-      </TouchableHighlight>
-    );
-  },
-  lapButton: function() {
-    return(
-      <TouchableHighlight 
-        style={styles.button}
-        underlayColor={'grey'}
-        onPress={this.handleLapPress}>
-        <Text>
-          Lap
-        </Text>
-      </TouchableHighlight>
-    );
-  },
-  handleLapPress: function() {
-    var lap = this.state.timeElapsed;
-
-    this.setState({
-      startTime: new Date(),
-      laps: this.state.laps.concat([lap])
-    });
-  },
-  handleStartPress: function() {
-    console.log('start was pressed');
-    if(this.state.running) {
-      clearInterval(this.interval);
-      this.setState({
-        running: false
-      });
-      return;
-    }
-
-    this.setState({
-      startTime: new Date()
-    });
-
-    this.interval = setInterval(() => {
-      this.setState({
-        timeElapsed: new Date() - this.state.startTime,
-        running: true
-      });
-    }, 50)
-  }
+  _onPressButtonGET: function() {
+        fetch(url, {method: "GET"})
+        .then((response) => response.json())
+        .then((responseData) => {
+            Alert.alert(
+                "GET Response",
+                "City/Nearest City -> " + JSON.stringify(responseData.name)
+            )
+        })
+        .done();
+    },
+    render: function() {
+        return (
+            <View style={styles.container}>
+                <TouchableHighlight onPress={this._onPressButtonGET} style={styles.button}>
+                    <Text>GET City</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    },  
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'stretch'
-  },
-  header: {
-    flex: 1
-  },
-  footer: {
-    flex: 1
-  },
-  timerWrapper: {
-    flex: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  bottomWrapper: {
-    flex: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  timer: {
-    fontSize: 60
-  },
-  button: {
-    borderWidth: 2,
-    height: 100,
-    width: 100,
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  startButton: {
-    borderColor: '#00CC00'
-  },
-  stopButton: {
-    borderColor: '#CC0000'
-  },
-  lap: {
-    justifyContent: 'space-around',
-    flexDirection: 'row'
-  },
-  lapText: {
-    fontSize: 30
-  }
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        backgroundColor: '#eeeeee',
+        padding: 10,
+        marginRight: 5,
+        marginLeft: 5,
+    }
 });
  
 AppRegistry.registerComponent('sample', () => sample);
