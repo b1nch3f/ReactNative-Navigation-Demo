df3a42ab5362254b8b4a8750c28ba8ebfecb1e3f
sangram kesari ray <shankar.ray030@gmail.com>
2016-07-24 02:47:07 +0530

    final

--- index.android.js
+++ index.android.js
@@ -1,63 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
 'use strict';
- 
-var React = require('react');
-var ReactNative = require('react-native');
- 
-var {
-  Alert,
-  AppRegistry,
-  Text,
-  TouchableHighlight,
-  View,
-  StyleSheet
-} = ReactNative;
 
-var region = {
-  latitude: 0,
-  longitude: 0
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Navigator,
+  TouchableOpacity,
+} from 'react-native';
+
+var SplashPage = require('./SplashPage');
+var LoginPage = require('./LoginPage');
+var MainPage = require('./MainPage');
+var PersonPage = require('./PersonPage');
+var NoNavigatorPage = require('./NoNavigatorPage');
+
+class sample extends Component {
+  render() {
+    return (
+      <Navigator
+          initialRoute={{id: 'SplashPage', name: 'Index'}}
+          renderScene={this.renderScene.bind(this)}
+          configureScene={(route) => {
+            if (route.sceneConfig) {
+              return route.sceneConfig;
+            }
+            return Navigator.SceneConfigs.FloatFromRight;
+          }} />
+    );
+  }
+  renderScene(route, navigator) {
+    var routeId = route.id;
+    if (routeId === 'SplashPage') {
+      return (
+        <SplashPage
+          navigator={navigator} />
+      );
+    }
+    if (routeId === 'LoginPage') {
+      return (
+        <LoginPage
+          navigator={navigator} />
+      );
+    }
+    if (routeId === 'MainPage') {
+      return (
+        <MainPage
+            navigator={navigator} />
+      );
+    }
+    if (routeId === 'PersonPage') {
+      return (
+        <PersonPage
+          navigator={navigator} />
+      );
+    }
+    if (routeId === 'NoNavigatorPage') {
+      return (
+        <NoNavigatorPage
+            navigator={navigator} />
+      );
+    }
+    return this.noRoute(navigator);
+
+  }
+  noRoute(navigator) {
+    return (
+      <View style={{flex: 1, alignItems: 'stretch', justifyContent: 'center'}}>
+        <TouchableOpacity style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+            onPress={() => navigator.pop()}>
+          <Text style={{color: 'red', fontWeight: 'bold'}}>请在 index.js 的 renderScene 中配置这个页面的路由</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
-var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=xxxx';
-
-var url = `${rootUrl}&lat=${region.latitude}&lon=${region.longitude}`;
- 
-var sample = React.createClass({
-  _onPressButtonGET: function() {
-        fetch(url, {method: "GET"})
-        .then((response) => response.json())
-        .then((responseData) => {
-            Alert.alert(
-                "GET Response",
-                "City/Nearest City -> " + JSON.stringify(responseData.name)
-            )
-        })
-        .done();
-    },
-    render: function() {
-        return (
-            <View style={styles.container}>
-                <TouchableHighlight onPress={this._onPressButtonGET} style={styles.button}>
-                    <Text>GET City</Text>
-                </TouchableHighlight>
-            </View>
-        );
-    },  
-});
-
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    button: {
-        backgroundColor: '#eeeeee',
-        padding: 10,
-        marginRight: 5,
-        marginLeft: 5,
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
- 
+
 AppRegistry.registerComponent('sample', () => sample);
